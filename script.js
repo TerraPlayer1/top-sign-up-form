625e52d3e9e692002f72cca659a7744d05448e44
TerraPlayer1 <terragamer70@gmail.com>
2023-01-16 11:28:47 +0200

    Make pass fields check if pass is the same

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
-const pass = document.getElementById("pass");
-const cPass = document.getElementById("confirm-pass");
+const pass = document.querySelector("#pass");
+const cPass = document.querySelector("#confirm-pass");
+const pcError = document.querySelector("#pass-confirm-error");
 
 cPass.oninput = async function() {
-  await new Promise(r => setTimeout(r, 700));
-
-   if (pass.value == cPass.value ) {
-    cPass.style.backgroundColor = "rgb(222,333,222)";
-  } else cPass.style.backgroundColor = "rgb(222,33,222)";
+  await new Promise(r => setTimeout(r, 500));
+  comparePass(pass,cPass)
 };
+
+
+pass.oninput = async function() {
+  await new Promise(r => setTimeout(r, 500));
+  comparePass(pass,cPass)
+};
+
+function comparePass(a, b) {
+  if (b.value.length == 0) {
+    return;
+  };
+
+  if (a.value !== b.value ) {
+    pass.style.borderColor = "red"
+    cPass.style.borderColor = "red"
+    pcError.textContent = "Passwords do not match" 
+  } else pass.style.borderColor = "", cPass.style.borderColor = "", pcError.textContent = "";
+}
